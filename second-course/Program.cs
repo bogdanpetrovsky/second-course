6e2ca18a7c260c08d1c6b841186313db9d5ef0a3
bogdanpetrovsky <bpetrovsky977@gmail.com>
2023-02-22 11:25:52 +0200

    added ~U calculation

--- second-course/Program.cs
+++ second-course/Program.cs
@@ -43,7 +43,7 @@ double Kernel12(double t1, double t2)
 {
     double kernelLeftValue = (VGamma2(t2)[0] * VGamma1(t1)[0] + VGamma2(t2)[1] * VGamma1(t1)[1]) /
                              GetEuclideanDistance(X1(t1)[0], X2(t2)[0], X1(t1)[1], X2(t2)[1]);
-    double kernelRightValueNominator = (X1(t1)[0] - X2(t2)[0] * VGamma2(t2)[0] + X1(t1)[1] - X2(t2)[1] * VGamma2(t2)[1]) *
+    double kernelRightValueNominator = (X1(t1)[0] - X2(t2)[0] * VGamma1(t2)[0] + X1(t1)[1] - X2(t2)[1] * VGamma1(t2)[1]) *
                                        (X1(t1)[0] - X2(t2)[0] * VGamma1(t1)[0] + X1(t1)[1] - X2(t2)[1] * VGamma1(t1)[1]);
     double kernelRightValue = kernelRightValueNominator / 
                               Math.Pow(GetEuclideanDistance(X1(t1)[0], X2(t2)[0], X1(t1)[1], X2(t2)[1]), 4);
@@ -127,9 +127,23 @@ double[] Gauss (double[,] a, int N) {
     return ans;
 }
 
-void Solve()
+double GetApproximatedU(double x1, double x2, double[] uValues, double N)
 {
-    int N = 4;
+    double s1 = 0, s2 = 0;
+    for (int i = 0; i < 2*N; i++)
+    {
+        double t = i * Math.PI / N;
+        s1 += uValues[i] * Math.Log(1 / GetEuclideanDistance(x1, X1(t)[0], x2, X1(t)[1])) * GetEuclideanDistance(Der1X1(t)[0], 0, Der1X1(t)[1], 0);
+        s2 += uValues[i] * GetEuclideanDistance(Der1X2(t)[0], 0, Der1X2(t)[1], 0) *
+              (((x1 - X2(t)[0]) * VGamma2(t)[0] + (x2 - X2(t)[1]) * VGamma2(t)[1]) / Math.Pow(GetEuclideanDistance(x1, X2(t)[0], x2, X2(t)[1]), 2));
+    }
+
+    return s1 / (2 * N) + s2 / (2 * N);
+}
+
+void Solve(int N1)
+{
+    int N = N1;
     double[] H_F_Values = new double [4*N];
     double[,] kernelMatrix = new double [4*N, 4*N];
     for (var i = 0; i < N*4; i++) { H_F_Values[i] = i < 2*N ? 0 : 1; }
@@ -138,14 +152,14 @@ void Solve()
     {
         for (int j = 0; j < 2*N; j++)
         {
-            kernelMatrix[i, j] = Kernel11(i * Math.PI/(2*N), j * Math.PI/(2*N));
-            kernelMatrix[i, 2*N + j] = Kernel12(i * Math.PI/(2*N), 2*N*j * Math.PI/(2*N));
-            kernelMatrix[2*N + i, j] = Kernel21(2*N*i * Math.PI/(2*N), j * Math.PI/(2*N));
-            kernelMatrix[2*N + i, 2*N + j] = Kernel22(2*N*i * Math.PI/(2*N), 2*N*j * Math.PI/(2*N));
+            kernelMatrix[i, j] = Kernel11(i * Math.PI/(2*N), j * Math.PI/(2*N))/(2*N);
+            kernelMatrix[i, 2*N + j] = Kernel12(i * Math.PI/(2*N), 2*N*j * Math.PI/(2*N))/(2*N);
+            kernelMatrix[2*N + i, j] = Kernel21(2*N*i * Math.PI/(2*N), j * Math.PI/(2*N))/(2*N);
+            kernelMatrix[2*N + i, 2*N + j] = Kernel22(2*N*i * Math.PI/(2*N), 2*N*j * Math.PI/(2*N))/(2*N);
         }
 
-        kernelMatrix[i, i] = kernelMatrix[i, i] / (2*N) - 0.5;
-        kernelMatrix[2*N + i, 2*N + i] = kernelMatrix[2*N + i, 2*N + i] / (2*N) - 0.5;
+        kernelMatrix[i, i] = kernelMatrix[i, i] - 0.5;
+        kernelMatrix[2*N + i, 2*N + i] = kernelMatrix[2*N + i, 2*N + i] - 0.5;
     }
 
     double[,] kernelMatrixExtended = new double[4*N, 4*N + 1];
@@ -156,8 +170,15 @@ void Solve()
     // double[,] testCase = new double[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } }; double[] ans = Gauss(testCase, 2); for (int j = 0; j < 2; j++) { Console.Write(ans[j] + " "); }
     
     double[] ans = Gauss(kernelMatrixExtended, 4 * N);
+    Console.WriteLine("Gauss Values:");
     for (int j = 0; j < 4 * N; j++) { Console.Write(ans[j] + " "); }
+    Console.WriteLine("\n");
+    
+    Console.WriteLine("~U Value: " + GetApproximatedU(0, 0, ans, N));
 }
 
-Solve();
+Solve(4);
+// Console.WriteLine();
+// Solve(8);
+
 Console.WriteLine("\nEND");
